fc930374c2eb55b2e8a30c731968257d8b2d4882
zebray <2092680295@qq.com>
2025-01-05 15:28:48 +0800

    Automatic commit.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include "gitm.h"
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    ERROR("Usage: gitm <command>\n");
-    return 1;
-  }
-  if (!strcmp(argv[1], "version")) {
-    printf("%s",
+    if (argc < 2) {
+        ERROR("Usage: gitm <command>\n");
+        return 1;
+    }
+    if (!strcmp(argv[1], "version")) {
+        printf("%s",
 #include "../icon.txt"
-    );
-  }
-  return 0;
+        );
+    }
+    if (!strcmp(argv[1], "init")) {
+        if (make_directory("../") == 0) {
+            printf("Haha\n");
+        }
+        else {
+            printf("Failed to make directory.\n");
+        }
+    }
+
+    return 0;
 }
